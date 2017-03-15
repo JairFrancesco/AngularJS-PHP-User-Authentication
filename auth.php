6fef6d9c077e503f4a30dbae92e8aa6c8422c396
Jair Huaman Canqui <jairfrancesco@gmail.com>
2017-03-15 06:20:43 -0500

    Add auth.php file

---
+++ auth.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Auth
+{
+    public function login()
+    {
+        if($this->input->is_ajax_request())
+        {
+            if(!$this->input->post("email") || !$this->input->post("password"))
+            {
+                echo json_encode(array("code" => 1, "response" => "Datos insuficientes"));
+            }
+            $email = $this->input->post("email");
+            $password = sha1($this->input->post("password"));
+            $this->load->model("auth_model");
+            $user = $this->auth_model->login($email, $password);
+            if($user !== false)
+            {
+                //ha hecho login
+                $user->iat = time();
+                $user->exp = time() + 300;
+                $jwt = JWT::encode($user, '');
+                echo json_encode(
+                    array(
+                        "code" => 0,
+                        "response" => array(
+                            "token" => $jwt
+                        )
+                    )
+                );
+            }
+        }
+    }
+    else
+    {
+        show_404();
+    }
+}
+/* End of file auth.php */
